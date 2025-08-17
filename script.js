e4d2b01a2ed206b9ba82f04603c5b17aba2741fd
GabrielRossiSoares <gabisrossi2002@gmail.com>
2025-08-16 23:40:28 -0400

    removido informações comentadas

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// function toggleMode() {
-//   const html = document.documentElement
-
-//   // if (html.classList.contains("light")) {
-//   //   html.classList.remove("light")
-//   //} else {
-//   //   html.classList.add("light")
-//   // }
-
-//   html.classList.toggle("light")
-// }
-
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
